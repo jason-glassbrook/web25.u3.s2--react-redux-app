1fd01f07087ed03e674af8edb4094ff4e0d78750
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-12 18:08:38 -0500

    build boilerplate app

---
+++ src/states/_/index.js
@@ -0,0 +1,13 @@
+import init from './init';
+import context from './context';
+import actions from './actions';
+import reducer from './reducer';
+import store from './store';
+
+export default {
+  init,
+  context,
+  actions,
+  reducer,
+  store,
+};
