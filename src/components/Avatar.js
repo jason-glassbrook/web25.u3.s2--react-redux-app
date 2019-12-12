bef5747dc96a8e2e398d85a15bc62acae43b1fae
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-12 18:45:32 -0500

    import components and states

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,12 @@
 /// external modules ///
 import React from 'react';
+import ReactRedux from 'react-redux';
+
+/// components ///
+import Loader from 'react-loader-spinner';
+
+/// states ///
+import { actions } from 'states/avatar'
 
 /***************************************
   MAIN
