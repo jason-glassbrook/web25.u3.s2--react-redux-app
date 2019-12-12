3c6c4ce2f62d042edc53baaf4b8118ce77f7c925
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-12 18:26:11 -0500

    import packages and state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 /// external modules ///
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-/// internal modules ///
+/// components ///
 import App from './App';
+
+/// states ///
+import avatar from 'states/avatar';
 
 /// styles ///
 import 'reset-css/reset.css';
