e0c29af04cf87dd88437ebc24cb2fd7f2f24e15a
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-12 18:36:57 -0500

    add blank Avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,15 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  MAIN
+***************************************/
+const Avatar = (props) => {
+  return (
+    <div className='Avatar'>Avatar</div>
+  );
+};
+
+/**************************************/
+
+export default Avatar;
